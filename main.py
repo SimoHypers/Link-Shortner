db02386cab008067aae5adeda45bc5b2f9862a18
Simo <153776287+SimoHypers@users.noreply.github.com>
2025-01-11 22:18:07 +0400

    Added copy URL to clipboard functionality via button

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout)
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QClipboard
 from dotenv import load_dotenv
 import os
 
@@ -17,6 +17,7 @@ class LinkShortner(QWidget):
         self.link_name_input = QLineEdit(self)
         self.get_link_button = QPushButton("Get Link", self)
         self.short_link_label = QLabel(self)
+        self.copy_url_button = QPushButton("Copy URL to Clipboard", self)
         self.initUI()
 
     def initUI(self):
@@ -30,6 +31,7 @@ class LinkShortner(QWidget):
         vbox.addWidget(self.link_name_input)
         vbox.addWidget(self.get_link_button)
         vbox.addWidget(self.short_link_label)
+        vbox.addWidget(self.copy_url_button)
 
         self.setLayout(vbox)
 
@@ -38,12 +40,14 @@ class LinkShortner(QWidget):
         self.link_name_label.setAlignment(Qt.AlignCenter)
         self.link_name_input.setAlignment(Qt.AlignCenter)
         self.short_link_label.setAlignment(Qt.AlignCenter)
+
         self.original_link_label.setObjectName("original_link_label")
         self.original_link_input.setObjectName("original_link_input")
         self.link_name_label.setObjectName("link_name_label")
         self.link_name_input.setObjectName("link_name_input")
         self.get_link_button.setObjectName("get_link_button")
         self.short_link_label.setObjectName("short_link_label")
+        self.copy_url_button.setObjectName("copy_url_button")
 
         self.setStyleSheet("""
             QLabel, QPushButton {
@@ -73,11 +77,18 @@ class LinkShortner(QWidget):
             QLabel#short_link_label {
                     font-size: 50px;
                 }
+            QPushButton#copy_url_button {
+                    font-size: 15px;
+                    font-weight: bold;
+                    padding: 5px;
+                }
 
             """
         )
 
         self.get_link_button.clicked.connect(self.shorten_link)
+        self.copy_url_button.clicked.connect(self.copyToClipBoard)
+
 
     def shorten_link(self):
         original_link = self.original_link_input.text().strip()
@@ -112,7 +123,7 @@ class LinkShortner(QWidget):
             if status == 7:  # Success
                 title = data['url'].get('title', 'No title')
                 short_link = data['url']['shortLink']
-                self.short_link_label.setText(f"Short Link: {short_link}")
+                self.short_link_label.setText(f"{short_link}")
                 print(f"Title: {title}")
                 print(f"Short Link: {short_link}")
             else:
@@ -129,7 +140,11 @@ class LinkShortner(QWidget):
         except Exception as e:
             self.short_link_label.setText("An error occurred. Try again.")
             print(f"Exception: {e}")
-
+    
+    def copyToClipBoard(self):
+        clipboard = QApplication.clipboard()
+        clipboard.setText(self.short_link_label.text())
+        self.short_link_label.setText("Link copied to clipboard!")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
